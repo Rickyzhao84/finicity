63d06a0f9d5c62dd2b8d2427d9effb517a40a501
rickyzhao84 <rickyzhao84@gmail.com>
2024-05-31 08:41:01 -0700

    Adding

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import BankWidget from './components/BankWidget';
+import ConnectWidget from './components/ConnectWidget';
 
-function App() {
+const App = () => {
+  const customerId = '7029334956';
+  const partnerId = '2445584535123';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bank Account Balances</h1>
+      <ConnectWidget partnerId={partnerId} customerId={customerId} />
+      <BankWidget customerId={customerId} />
     </div>
   );
-}
+};
 
 export default App;
